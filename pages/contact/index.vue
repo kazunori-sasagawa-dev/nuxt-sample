85e91c250aee6bbc048c52e6fb6addc74186b485
笹川数典 <k.sasagawa@sasakawasuutennoMacBook-Air.local>
2023-06-20 21:56:23 +0900

    validation

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -3,48 +3,75 @@
     <h2 class="p-contact__title">お問い合わせ</h2>
     <p class="p-contact__lead">フォームに必要事項を入力し、<br class="switch__display--sp" />送信ボタンをおしてください。</p>
     <div class="p-contact__contents">
-      <form method="POST" name="contact" data-netlify="true" class="p-contact__form">
-        <input type="hidden" name="form-name" value="contact" />
+      <ValidationObserver ref="obs" v-slot="{ invalid, validated }" tag="div">
+        <form method="POST" name="contact" data-netlify="true" netlify-honeypot="bot-field" class="p-contact__form">
+          <input type="hidden" name="form-name" value="contact" />
 
-        <div class="p-contact__item">
-          <label for="username" class="p-contact__label">お名前</label>
-          <input type="text" id="username" name="username" autocomplete="username" class="p-contact__input">
-        </div>
+          <div class="p-contact__item">
+            <label for="username" class="p-contact__label">お名前</label>
+            <ValidationProvider v-slot="{ errors }" rules="required|max:100" name="username">
+              <input v-model="username" type="text" id="username" name="username" autocomplete="username" class="p-contact__input">
+              <p v-if="errors.length" class="p-contact__errorText">{{ errors[0] }}</p>
+            </ValidationProvider>
+          </div>
 
-        <div class="p-contact__item">
-          <label for="katakana" class="p-contact__label">フリガナ</label>
-          <input type="text" id="katakana" name="katakana" autocomplete="katakana" class="p-contact__input">
-        </div>
+          <div class="p-contact__item">
+            <label for="katakana" class="p-contact__label">フリガナ</label>
+            <ValidationProvider v-slot="{ errors }" rules="required" name="katakana">
+              <input v-model="katakana" type="text" id="katakana" name="katakana" autocomplete="katakana" class="p-contact__input">
+              <p v-if="errors.length" class="p-contact__errorText">{{ errors[0] }}</p>
+            </ValidationProvider>
+          </div>
 
-        <div class="p-contact__item">
-          <label for="usermail" class="p-contact__label">メールアドレス</label>
-          <input type="text" id="usermail" name="usermail" autocomplete="usermail" class="p-contact__input">
-        </div>
+          <div class="p-contact__item">
+            <label for="usermail" class="p-contact__label">メールアドレス</label>
+            <ValidationProvider v-slot="{ errors }" rules="required|email|max:256" name="usermail">
+              <input v-model="usermail" type="text" id="usermail" name="usermail" autocomplete="usermail" class="p-contact__input">
+              <p v-if="errors.length" class="p-contact__errorText">{{ errors[0] }}</p>
+            </ValidationProvider>
+          </div>
 
-        <div class="p-contact__item">
-          <label for="message" class="p-contact__label">お問い合わせ内容</label>
-          <textarea id="message" name="message" rows="5" class="p-contact__textarea"></textarea>
-        </div>
+          <div v-show="false" class="p-contact__item">
+            <input type="text" name="bot-field" />
+          </div>
 
-        <div class="p-contact__item">
-          <button type="submit" class="p-contact__submit isActive">送信</button>
-        </div>
-      </form>
+          <div class="p-contact__item">
+            <label for="message" class="p-contact__label">お問い合わせ内容</label>
+            <ValidationProvider v-slot="{ errors }" rules="required|max:2000" name="message">
+              <textarea v-model="message" id="message" name="message" rows="5" class="p-contact__textarea"></textarea>
+              <p v-if="errors.length" class="p-contact__errorText">{{ errors[0] }}</p>
+            </ValidationProvider>
+          </div>
+
+          <div class="p-contact__item">
+            <button type="submit" :disabled="invalid || !validated" :class="invalid || !validated ? 'p-contact__submit' : 'p-contact__submit isActive'">送信</button>
+          </div>
+        </form>
+      </ValidationObserver>
     </div>
   </section>
 </template>
 
 <script>
+import { ValidationObserver, ValidationProvider } from 'vee-validate';
 import Meta from '@/mixins/meta_01';
 
 export default {
+  components: {
+    ValidationObserver,
+    ValidationProvider
+  },
   mixins: [Meta],
   data() {
     return {
       meta: {
         title: 'お問い合わせ',
         description: 'お問い合わせページ'
-      }
+      },
+      username: '',
+      katakana: '',
+      usermail: '',
+      message: ''
     }
   }
 }
